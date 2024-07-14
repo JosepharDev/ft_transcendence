e664751412cac68d791a6637b5a6c9ffdb983784
Achraf-ahrach <achrafahrach44@gmail.com>
2024-07-14 12:06:31 +0100

    achraf update home page only

--- static/spajs/views/home.js
+++ static/spajs/views/home.js
@@ -34,12 +34,30 @@ function homeHtml()
 
     <div class="row">
 
-         <div class="col-lg-4 col-md-6 col-sm-12 mb-4 bobo">
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4 bobo">
             <div class="card h-100 bg-lightgreen small-card">
-                <div class="card-body d-flex flex-column justify-content-center text-center">
+                <div class="card-body d-flex flex-column justify-content-center text-center local">
                     <a href="/localgame" class="btn btn-primary mt-auto btn-sm dodo" data-localize="local">${translations[dataGlobal.selectedLanguage]['local']}</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4 bobo">
+            <div class="card h-100 bg-lightgreen small-card">
+                <div class="card-body d-flex flex-column justify-content-center text-center online1">
                     <a href="/remote" class="btn btn-primary mt-auto btn-sm dodo" data-localize="online1">${translations[dataGlobal.selectedLanguage]['online1']}</a>
+                </div>
+            </div>
+        </div>  
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4 bobo">
+            <div class="card h-100 bg-lightgreen small-card">
+                <div class="card-body d-flex flex-column justify-content-center text-center online2">
                     <a href="/remote4" class="btn btn-primary mt-auto btn-sm dodo" data-localize="online2">${translations[dataGlobal.selectedLanguage]['online2']}</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-12 mb-4 bobo">
+            <div class="card h-100 bg-lightgreen small-card">
+                <div class="card-body d-flex flex-column justify-content-center text-center tournament">
                     <a href="/tournament" class="btn btn-primary mt-auto btn-sm dodo" data-localize="tournament">${translations[dataGlobal.selectedLanguage]['tournament']}</a>
                 </div>
             </div>
