f5c575611b671368632656436e323efd4f6d33b3
lazrak <z.zakarialazrak@gmail.com>
2024-07-16 10:15:39 +0100

    front

---
+++ Frontend/static/spajs/utils/urlRoute.js
@@ -0,0 +1,37 @@
+export function urlRoute (event)
+{
+    event = event || window.event;
+
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    deleteEvent = [];
+    urlLocationHandler();
+}
+
+export function pushUrl (href)
+{
+    try
+    {
+        if (href === window.location.pathname)
+            return ;
+        console.log(`-> ${href}   ${window.location.pathname}`)
+
+
+        if (history.state && history.state.href === href) {
+            history.replaceState({ href }, '', href);
+        } else {
+            history.pushState({ href }, '', href);
+        }
+        console.log('-> ${href}   ${window.location.pathname}')
+        console.log(window.location.pathname)
+        
+        // history.pushState({}, '', href);
+
+        
+        window.dispatchEvent(new Event('popstate'));
+    }
+    catch (err)
+    {
+        console.log("BAL");
+    }
+}
---
+++ Frontend/static/spajs/views/online.js
@@ -0,0 +1,16 @@
+import { dataGlobal } from "./globalData.js";
+export function sendOnline()
+{
+    if (dataGlobal.sentOnline)
+        return ;
+    console.log("SENDONLINE");
+    dataGlobal.socketOnline = new WebSocket(
+        'wss://'
+        + window.location.host
+        + '/ws/onlineUser/'
+        + '3'
+        + '/'
+    );
+    dataGlobal.sentOnline = true;
+
+}
